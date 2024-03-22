81a113389bf5b1428b53c1864efdd19f4c5bba2c
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-03-22 12:39:38 +0530

    new

--- src/sections/RoadMap.jsx
+++ src/sections/RoadMap.jsx
@@ -10,51 +10,56 @@ function RoadMap() {
       />
 
       <div className="container-wrapper mb-14">
-      <img
-          src="images/parasuit.svg"
-          alt=""
-          className=" mx-auto h-[100px]  relative z-10"
-        />
-        <h2 className="font-groot-one text-5xl my-2 text-[#000000] text-center font-bold relative z-30 -mt-4">
-         Our  Roadmap
+    
+        <h2 className="font-groot-two text-5xl my-2 text-[#0058bc] text-center font-bold relative z-30 my-4">
+         MarioSol Features
         </h2>
      
 
   
 
         <div className="flex justify-between flex-col sm:flex-row gap-5">
-          <div className="bg-[#2a4474] border-4  py-5 px-5  rounded-xl w-[100%] h-[190px] ">
+          <div className="pattern--hashes border-4  py-5 px-5  rounded-xl w-[100%] ">
             <div>
-            <h1 className="font-groot-one font-bold  text-white text-center  text-xl mb-1">Phase 1</h1>
+              <img src="images/nft.jpeg" alt="" className=' w-[300px] mx-auto rounded-xl mb-2' />
+            </div>
+            <div>
+            <h1 className="font-groot-two font-bold  text-white text-center  text-xl mb-1">Phase 1</h1>
              
             </div>
-            <p className=" text-lg font-popins font-normal lh-1_7">•  Build community foundation.</p>
-            <p  className=" text-lg font-popins font-normal lh-1_7">• Do initial market research.</p>
-            <p  className=" text-lg font-popins font-normal lh-1_7">•  Launch SylvesterSol.</p>
+            <p className=" text-lg font-popins font-normal lh-1_7 text-white">•  Build community foundation.</p>
+            <p  className=" text-lg font-popins font-normal lh-1_7 text-white">• Do initial market research.</p>
+            <p  className=" text-lg font-popins font-normal lh-1_7 text-white">•  Launch SylvesterSol.</p>
           
           
           </div>
 
-          <div className="bg-[#2a4474] border-4 py-5 px-5  rounded-xl w-[100%] h-full ">
+          <div className="pattern--hashes border-4 py-5 px-5  rounded-xl w-[100%] h-full ">
           <div>
-          <h1 className="font-groot-one font-bold  text-white text-center  text-xl mb-1">Phase 2</h1>
+              <img src="images/stake.jpeg" alt="" className=' w-[300px] mx-auto rounded-xl mb-2' />
+            </div>
+          <div>
+          <h1 className="font-groot-two font-bold  text-white text-center  text-xl mb-1">Phase 2</h1>
            
             </div>
-            <p className=" text-lg font-popins font-normal lh-1_7">• Grow community</p>
-            <p  className=" text-lg font-popins font-normal lh-1_7">• Do partnerships.</p>
-            <p  className=" text-lg font-popins font-normal lh-1_7">• CMC & CG listing.</p>
-            <p  className=" text-lg font-popins font-normal lh-1_7">• Introduce Staking. .</p>
+            <p className=" text-lg font-popins font-normal lh-1_7 text-white ">• Grow community</p>
+            <p  className=" text-lg font-popins font-normal lh-1_7 text-white">• Do partnerships.</p>
+            <p  className=" text-lg font-popins font-normal lh-1_7 text-white">• CMC & CG listing.</p>
+            <p  className=" text-lg font-popins font-normal lh-1_7 text-white">• Introduce Staking. .</p>
        
 
           </div>
-          <div className=" py-5 px-5  rounded-xl w-[100%] h-[190px] bg-[#2a4474] border-4">
+          <div className=" py-5 px-5  rounded-xl w-[100%] pattern--hashes border-4">
           <div>
-          <h1 className="font-groot-one font-bold  text-white text-center  text-xl mb-1">Phase 3</h1>
+              <img src="images/meme.jpeg" alt="" className=' w-[300px] mx-auto rounded-xl mb-2' />
+            </div>
+          <div>
+          <h1 className="font-groot-two font-bold  text-white text-center  text-xl mb-1">Phase 3</h1>
              
             </div>
-            <p className=" text-lg font-popins font-normal lh-1_7">•  Open Pet Store.</p>
-            <p  className=" text-lg font-popins font-normal lh-1_7">• List on Tier 1 Exchange.</p>
-            <p  className=" text-lg font-popins font-normal lh-1_7">• Launch SylvesterSol Game.</p>
+            <p className=" text-lg font-popins font-normal lh-1_7 text-white">•  Open Pet Store.</p>
+            <p  className=" text-lg font-popins font-normal lh-1_7 text-white">• List on Tier 1 Exchange.</p>
+            <p  className=" text-lg font-popins font-normal lh-1_7 text-white">• Launch SylvesterSol Game.</p>
           
 
           </div>
